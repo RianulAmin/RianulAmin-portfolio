1018a444ba7034a28eec740a17268e22a2f0b752
Rianul Amin <rianulamin.r@gmail.com>
2024-07-16 23:15:17 +0600

    updates on navbar

--- rianul-amin/src/components/Navbar.tsx
+++ rianul-amin/src/components/Navbar.tsx
@@ -1,9 +1,10 @@
 'use client';
 import Link from 'next/link';
 import { motion, MotionValue, useMotionValue } from 'framer-motion';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Navbar() {
+  const [activeLink, setActiveLink] = useState<string | null>(null);
 
   const links = [
     { href: '/projects', label: 'Projects' },
@@ -20,9 +21,9 @@ export default function Navbar() {
     const INPUT_RANGE = inputUpper - inputLower;
     const OUTPUT_RANGE = outputUpper - outputLower;
 
-    return(value: number) => 
+    return (value: number) =>
       outputLower + (((value - inputLower) / INPUT_RANGE) * OUTPUT_RANGE || 0);
-  }
+  };
 
   const setTransform = (item: HTMLElement & EventTarget, event: React.PointerEvent, x: MotionValue, y: MotionValue) => {
     const bounds = item.getBoundingClientRect();
@@ -32,7 +33,11 @@ export default function Navbar() {
     const yRange = mapRange(0, bounds.height, -1, 1)(relativeY);
     x.set(xRange * 15);
     y.set(yRange * 15);
-  }
+  };
+
+  const handleClick = (href: string) => {
+    setActiveLink(href);
+  };
 
   const MotionLink = motion(Link);
 
@@ -46,17 +51,26 @@ export default function Navbar() {
                 const x = useMotionValue(0);
                 const y = useMotionValue(0);
                 return (
-                  <motion.div key={link.href} className="relative" onPointerMove={(event) => {
-                    const item = event.currentTarget;
-                    setTransform(item, event, x, y);
-                  }}
-                  onPointerLeave={()=>{
-                    x.set(0);
-                    y.set(0);
-                  }}
-                  style={{ x, y }}
+                  <motion.div
+                    key={link.href}
+                    className="relative"
+                    onPointerMove={(event) => {
+                      const item = event.currentTarget;
+                      setTransform(item, event, x, y);
+                    }}
+                    onPointerLeave={() => {
+                      x.set(0);
+                      y.set(0);
+                    }}
+                    style={{ x, y }}
                   >
-                    <MotionLink href={link.href} className="font-medium text-m py-2 px-4 transition-all duration-500 ease-out">
+                    <MotionLink
+                      href={link.href}
+                      className={`font-medium text-m py-2 px-4 transition-all duration-500 ease-out ${
+                        activeLink === link.href ? 'active-dot' : 'hover-dot'
+                      }`}
+                      onClick={() => handleClick(link.href)}
+                    >
                       {link.label}
                     </MotionLink>
                   </motion.div>
