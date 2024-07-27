740724faaeb89b45ed3ee7321abc6c9d0824da26
Rianul Amin <rianulamin.r@gmail.com>
2024-07-28 05:37:07 +0600

    realistic clouds on the preloader

--- rianul-amin/src/components/Preloader/Preloader.tsx
+++ rianul-amin/src/components/Preloader/Preloader.tsx
@@ -10,58 +10,75 @@ export default function Preloader() {
         setDimension({ width: window.innerWidth, height: window.innerHeight });
     }, []);
 
-    const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height}
-                         Q${dimension.width * 0.95} ${dimension.height + 150}
-                         ${dimension.width * 0.90} ${dimension.height + 120}
-                         Q${dimension.width * 0.85} ${dimension.height + 160}
-                         ${dimension.width * 0.80} ${dimension.height + 100}
-                         Q${dimension.width * 0.75} ${dimension.height + 170}
-                         ${dimension.width * 0.70} ${dimension.height + 90}
-                         Q${dimension.width * 0.65} ${dimension.height + 150}
-                         ${dimension.width * 0.60} ${dimension.height + 80}
-                         Q${dimension.width * 0.55} ${dimension.height + 140}
-                         ${dimension.width * 0.50} ${dimension.height + 90}
-                         Q${dimension.width * 0.45} ${dimension.height + 160}
-                         ${dimension.width * 0.40} ${dimension.height + 100}
-                         Q${dimension.width * 0.35} ${dimension.height + 150}
-                         ${dimension.width * 0.30} ${dimension.height + 90}
-                         Q${dimension.width * 0.25} ${dimension.height + 160}
-                         ${dimension.width * 0.20} ${dimension.height + 100}
-                         Q${dimension.width * 0.15} ${dimension.height + 150}
-                         ${dimension.width * 0.10} ${dimension.height + 90}
-                         Q${dimension.width * 0.05} ${dimension.height + 120}
-                         0 ${dimension.height} Z`;
+    const initialPath1 = `M0 0 
+                        L${dimension.width} 0 
+                        L${dimension.width} ${dimension.height + 300} 
+                        A${dimension.width * 0.15} ${dimension.width * 0.15} 0 0 1 ${dimension.width * 0.82} ${dimension.height + 100} 
+                        A${dimension.width * 0.08} ${dimension.width * 0.08} 0 0 1 ${dimension.width * 0.62} ${dimension.height + 50} 
+                        A${dimension.width * 0.2} ${dimension.width * 0.2} 0 0 1 ${dimension.width * 0.52} ${dimension.height} 
+                        A${dimension.width * 0.25} ${dimension.width * 0.25} 0 0 1 ${dimension.width * 0.42} ${dimension.height + 50} 
+                        A${dimension.width * 0.3} ${dimension.width * 0.3} 0 0 1 ${dimension.width * 0.32} ${dimension.height + 75} 
+                        A${dimension.width * 0.15} ${dimension.width * 0.15} 0 0 1 ${-dimension.width * 0.1} ${dimension.height + 300} 
+                        L0 0`;
 
-    const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height}
-                        Q${dimension.width * 0.95} ${dimension.height - 150}
-                        ${dimension.width * 0.90} ${dimension.height - 120}
-                        Q${dimension.width * 0.85} ${dimension.height - 160}
-                        ${dimension.width * 0.80} ${dimension.height - 100}
-                        Q${dimension.width * 0.75} ${dimension.height - 170}
-                        ${dimension.width * 0.70} ${dimension.height - 90}
-                        Q${dimension.width * 0.65} ${dimension.height - 150}
-                        ${dimension.width * 0.60} ${dimension.height - 80}
-                        Q${dimension.width * 0.55} ${dimension.height - 140}
-                        ${dimension.width * 0.50} ${dimension.height - 90}
-                        Q${dimension.width * 0.45} ${dimension.height - 160}
-                        ${dimension.width * 0.40} ${dimension.height - 100}
-                        Q${dimension.width * 0.35} ${dimension.height - 150}
-                        ${dimension.width * 0.30} ${dimension.height - 90}
-                        Q${dimension.width * 0.25} ${dimension.height - 160}
-                        ${dimension.width * 0.20} ${dimension.height - 100}
-                        Q${dimension.width * 0.15} ${dimension.height - 150}
-                        ${dimension.width * 0.10} ${dimension.height - 90}
-                        Q${dimension.width * 0.05} ${dimension.height - 120}
-                        0 ${dimension.height} Z`;
+    const targetPath1 = `M0 0 
+                        L${dimension.width} 0 
+                        L${dimension.width} ${dimension.height - 600} 
+                        A${dimension.width * 0.15} ${dimension.width * 0.15} 0 0 1 ${dimension.width * 0.82} ${dimension.height - 400} 
+                        A${dimension.width * 0.08} ${dimension.width * 0.08} 0 0 1 ${dimension.width * 0.62} ${dimension.height - 450} 
+                        A${dimension.width * 0.2} ${dimension.width * 0.2} 0 0 1 ${dimension.width * 0.52} ${dimension.height - 500} 
+                        A${dimension.width * 0.25} ${dimension.width * 0.25} 0 0 1 ${dimension.width * 0.42} ${dimension.height - 450} 
+                        A${dimension.width * 0.3} ${dimension.width * 0.3} 0 0 1 ${dimension.width * 0.32} ${dimension.height - 425} 
+                        A${dimension.width * 0.15} ${dimension.width * 0.15} 0 0 1 ${-dimension.width * 0.1} ${dimension.height - 600} 
+                        L0 0`;
 
-    const curve = {
+    const initialPath2 = `M0 0 
+                        L${dimension.width} 0 
+                        L${dimension.width} ${dimension.height + 650} 
+                        A${dimension.width * 0.14} ${dimension.width * 0.14} 0 0 1 ${dimension.width * 0.78} ${dimension.height + 400} 
+                        A${dimension.width * 0.1} ${dimension.width * 0.1} 0 0 1 ${dimension.width * 0.68} ${dimension.height + 350} 
+                        A${dimension.width * 0.18} ${dimension.width * 0.18} 0 0 1 ${dimension.width * 0.58} ${dimension.height + 250} 
+                        A${dimension.width * 0.2} ${dimension.width * 0.2} 0 0 1 ${dimension.width * 0.48} ${dimension.height + 300} 
+                        A${dimension.width * 0.12} ${dimension.width * 0.12} 0 0 1 ${dimension.width * 0.38} ${dimension.height + 250} 
+                        A${dimension.width * 0.14} ${dimension.width * 0.14} 0 0 1 ${dimension.width * 0.28} ${dimension.height + 300} 
+                        A${dimension.width * 0.16} ${dimension.width * 0.16} 0 0 1 ${dimension.width * 0.18} ${dimension.height + 400} 
+                        A${dimension.width * 0.16} ${dimension.width * 0.16} 0 0 1 ${dimension.width * 0.08} ${dimension.height + 550} 
+                        A${dimension.width * 0.1} ${dimension.width * 0.1} 0 0 1 ${-dimension.width * 0.1} ${dimension.height + 650} 
+                        L0 0`;
+
+    const targetPath2 = `M0 0 
+                        L${dimension.width} 0 
+                        L${dimension.width} ${dimension.height - 550} 
+                        A${dimension.width * 0.14} ${dimension.width * 0.14} 0 0 1 ${dimension.width * 0.78} ${dimension.height - 300} 
+                        A${dimension.width * 0.1} ${dimension.width * 0.1} 0 0 1 ${dimension.width * 0.68} ${dimension.height - 350} 
+                        A${dimension.width * 0.18} ${dimension.width * 0.18} 0 0 1 ${dimension.width * 0.58} ${dimension.height - 450} 
+                        A${dimension.width * 0.2} ${dimension.width * 0.2} 0 0 1 ${dimension.width * 0.48} ${dimension.height - 400} 
+                        A${dimension.width * 0.12} ${dimension.width * 0.12} 0 0 1 ${dimension.width * 0.38} ${dimension.height - 450} 
+                        A${dimension.width * 0.14} ${dimension.width * 0.14} 0 0 1 ${dimension.width * 0.28} ${dimension.height - 400} 
+                        A${dimension.width * 0.16} ${dimension.width * 0.16} 0 0 1 ${dimension.width * 0.18} ${dimension.height - 300} 
+                        A${dimension.width * 0.16} ${dimension.width * 0.16} 0 0 1 ${dimension.width * 0.08} ${dimension.height - 150} 
+                        A${dimension.width * 0.1} ${dimension.width * 0.1} 0 0 1 ${-dimension.width * 0.1} ${dimension.height - 550} 
+                        L0 0`;
+
+    const curve1 = {
         initial: {
-            d: initialPath,
-            transition: { duration: 2, ease: [0.76, 0, 0.24, 1] }
+            d: initialPath1,
+            transition: { duration: 2.3, ease: [0.76, 0, 0.24, 1] }
         },
         exit: {
-            d: targetPath,
-            transition: { duration: 2, ease: [0.76, 0, 0.24, 1], delay: 0.3 }
+            d: targetPath1,
+            transition: { duration: 2.3, ease: [0.76, 0, 0.24, 1], delay: 0.3 }
+        }
+    };
+
+    const curve2 = {
+        initial: {
+            d: initialPath2,
+            transition: { duration: 2.3, ease: [0.76, 0, 0.24, 1] }
+        },
+        exit: {
+            d: targetPath2,
+            transition: { duration: 2.3, ease: [0.76, 0, 0.24, 1], delay: 0.3 }
         }
     };
 
@@ -80,7 +97,8 @@ export default function Preloader() {
             {dimension.width > 0 &&
                 <>
                     <svg>
-                        <motion.path variants={curve} initial="initial" exit="exit" fill="black"></motion.path>
+                        <motion.path className={styles.layer1} variants={curve2} initial="initial" exit="exit"></motion.path>
+                        <motion.path className={styles.layer2} variants={curve1} initial="initial" exit="exit"></motion.path>
                     </svg>
                     <p className={styles.loaderText}>ehono bhabinai ki boshamu</p>
                 </>
